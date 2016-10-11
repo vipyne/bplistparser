16aaa2234723116467e8eb7389c4b2b95983018e
vipyne <vipyne@gmail.com>
2016-10-11 15:57:46 -0400

    some cleanup

--- bplist-parser.c
+++ bplist-parser.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <stdint.h>
+#include <string.h>
 
 uint32_t read_big_endian_32 (FILE* file)
 {
@@ -19,6 +20,7 @@ uint32_t read_big_endian_32 (FILE* file)
 		return output;
 	}
 	errno = ENOENT;
+	printf("eof error %d, file error %d\n", feof(file), ferror(file));
 	return 0;
 }
 
@@ -31,11 +33,9 @@ int main (int argc, char *argv[])
 	}
 
 	FILE *filePointer;
-	
 	// r - read
 	// b - binary mode
 	filePointer = fopen(argv[1], "rb");
-
 	if (filePointer == NULL)
 	{
 		printf("file not readable because: %d\n", errno);
@@ -43,28 +43,37 @@ int main (int argc, char *argv[])
 	}
 
 	char firstEightBytes[9];
-	
 	// set null terminator
 	firstEightBytes[8] = 0;
 	fread(&firstEightBytes, 1, 8, filePointer);
 	
-	printf("first 8 bytes are: %s\n", firstEightBytes); 
 	
+	int res = strncmp(firstEightBytes, "bplist00", 8);
+	
+	if (res)
+	{
+		printf("result: %d\n", res);
+		printf("first 8 bytes are: %s\n", firstEightBytes); 
+		return 1;
+	}	
+
 	long e_offsetFromEnd = -4; // reading from end of file, so must be negative number
 	int seekResult = fseek(filePointer, e_offsetFromEnd, SEEK_END);
+	if (seekResult)
+	{ 
+		printf("seek result: %d\n", seekResult);
+		return 1;
+	}
 
-	printf("seek result: %d\n", seekResult);
-	
+	// reading bytes in big endian - reading in the offset of offset table
 	errno = 0;
-	uint32_t b_offsetOfOffsetTable = read_big_endian_32(filePointer);
+	uint32_t offsetOfOffsetTable = read_big_endian_32(filePointer);
 	if (errno == 0)
 	{
-		printf("offset of offset table: %d\n", b_offsetOfOffsetTable);
+		printf("offset of offset table: %d\n", offsetOfOffsetTable);
 	} else {
 		printf("error: %d\n", errno);
-	}
-		
-	printf("eof error %d, file error %d\n", feof(filePointer), ferror(filePointer));
+	}		
 
 	return 0;
 }
